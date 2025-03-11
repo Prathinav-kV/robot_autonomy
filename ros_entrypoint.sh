f859927315678cdda73a16438fa414358824a8f4
Prathinav-kV <prathinav.kv@gmail.com>
2025-03-10 23:15:59 -0400

    Changed the ros_entrypoint.sh script

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,6 +3,7 @@ source /opt/ros/humble/setup.bash
 source /ros2_ws/install/setup.bash
 
 # Export necessary paths for Gazebo and TurtleBot3
+export TURTLEBOT3_MODEL=waffle
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_gazebo/models
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_description/meshes
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins
